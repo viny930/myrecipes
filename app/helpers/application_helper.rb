e17491c93ef796c5e0340cfbef3187e79d4ead8c
vmrao33@gmail.com <vmrao33@gmail.com>
2016-02-14 04:20:09 +0000

    routes and gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def gravatar_for(chef, options = {size:80})
+        gravatar_id=Digest::MD5::hexdigest(chef.email.downcase)
+        size=options[:size]
+        gravatar_url="https://secure.gravator.com/avatar/#{gravatar_id}?size=#{size}"
+        image_tag(gravatar_url, alt: chef.chefname, class: "gravatar")
+    end
 end
